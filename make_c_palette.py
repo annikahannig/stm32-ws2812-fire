1e940e10d7935be7434d4a63369d2788951fde67
Matthias Hannig <matthias@hannig.cc>
2017-12-07 22:09:05 +0100

    added fire palette

--- make_c_palette.py
+++ make_c_palette.py
@@ -1,15 +1,29 @@
 
 import colorcet
 
+pal = colorcet.fire
+
+
 def hex2rgb(val):
     return tuple(int(val[i:i+2], 16) for i in (0, 2 ,4))
 
 
-print("uint8_t fire_palette[] = [")
-for v in colorcet.fire:
+print("#ifndef FIRE_PALETTE_H")
+print("#define FIRE_PALETTE_H")
+print()
+
+print("uint8_t fire_palette[] = {")
+for v in pal[:-1]:
     c = hex2rgb(v[1:])
     print("{}, {}, {},".format(c[0], c[1], c[2]))
 
-print("];")
 
+c = hex2rgb(pal[-1][1:])
+print("{}, {}, {}".format(c[0], c[1], c[2]))
 
+print("};")
+
+print()
+print("#endif")
+
+print()
